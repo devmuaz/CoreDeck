b313fe571bf6328fecb03602b9abef48a96475ab
AbdulMuaz Aqeel <devmuaz@gmail.com>
2026-05-03 12:43:26 +0300

    Redesign AVD creation, device profiles, image selection, storage details, and emulator options. Add richer system image metadata parsing and improve Windows emulator stop detection.

---
+++ src/gui/windows/device_profile.h
@@ -0,0 +1,20 @@
+//
+// Created by AbdulMuaz Aqeel on 02/05/2026.
+//
+
+#ifndef COREDECK_DEVICE_PROFILE_H
+#define COREDECK_DEVICE_PROFILE_H
+
+#include <string>
+
+#include "../context.h"
+
+namespace CoreDeck {
+    DeviceCategory DeviceCategoryForProfile(const DeviceProfile &device);
+
+    std::string DeviceProfilePreviewLabel(const DeviceProfile &device);
+
+    void BuildDeviceProfileWindow(Context &context);
+}
+
+#endif // COREDECK_DEVICE_PROFILE_H
